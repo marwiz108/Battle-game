475b8d9a363fbc51f745616c5c7631184c0ca3ea
Marwa Elali <marwaelali@gmail.com>
2018-06-18 12:05:33 +0100

    implementing hit points - attacking reduces HPs

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,19 @@
 class Player
-  attr_reader :name
 
-  def initialize(name)
+  DEFAULT_HP = 50
+
+  attr_reader :name, :hit_points
+
+  def initialize(name, hit_points=DEFAULT_HP)
     @name = name
+    @hit_points = hit_points
+  end
+
+  def attack(opponent)
+    opponent.damaged
+  end
+
+  def damaged
+    @hit_points -= 10
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:marwa) { Player.new('Marwa') }
+  subject(:computer) { Player.new('Computer') }
 
   describe '#name' do
     it 'returns the name' do
-      expect(marwa.name).to eq('Marwa')
+      expect(marwa.name).to eq 'Marwa'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(marwa.hit_points).to eq Player::DEFAULT_HP
+    end
+  end
+
+  describe '#attack' do
+    it 'hits opponent' do
+      expect(computer).to receive(:damaged)
+      marwa.attack(computer)
+    end
+  end
+
+  describe '#damaged' do
+    it 'reduces hit points by 10' do
+      expect { computer.damaged }.to change { computer.hit_points }.by -10
     end
   end
 end
